4ef5d637c3749cf01e9d162dd781fc0aea4db0cc
fausutus <bakhome14@gmail.com>
2023-09-30 14:24:09 +0300

    Fix

--- src/Lab1/Lab1.clj
+++ src/Lab1/Lab1.clj
@@ -2,14 +2,15 @@
 
 (defn repeated [coll n]
   (loop [seen {}
-         xs (seq coll)
-         result []]
-    (if-let [[y & ys] xs]
-      (let [current-count (get seen y 0)]
-        (recur (update seen y (fn [x] (if x (inc x) 1))) ys
-               (if (>= current-count n) (conj result y) result)))
-      result)))
-
+         xs coll
+         result-seq []]
+    (if-let [[y & ys] (seq xs)]
+      (recur (update seen y (fnil inc 0))
+             ys
+             (if (= (get seen y) n)
+               (conj result-seq y)
+               result-seq))
+      (lazy-seq result-seq))))
 (defn main []
   (println "Введите элементы списка (через пробел), затем n:")
   (let [input (read-line)
@@ -17,4 +18,3 @@
         n (-> (read-line) Integer.)]
     (prn (repeated parsed-input n) )))
 (main)
-
