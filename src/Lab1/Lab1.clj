3e7324d8370c24d9db1623f1c0aa19c14e39d170
fausutus <bakhome14@gmail.com>
2023-09-30 15:14:41 +0300

    Lazy Seq !

--- src/Lab1/Lab1.clj
+++ src/Lab1/Lab1.clj
@@ -1,16 +1,17 @@
 (ns Lab1.Lab1)
 
 (defn repeated [coll n]
-  (loop [seen {}
-         xs coll
-         result-seq []]
-    (if-let [[y & ys] (seq xs)]
-      (recur (update seen y (fnil inc 0))
-             ys
-             (if (= (get seen y) n)
-               (conj result-seq y)
-               result-seq))
-      (lazy-seq result-seq))))
+  (lazy-seq
+    (loop [seen {}
+           xs coll
+           result-seq []]
+      (if-let [[y & ys] (seq xs)]
+        (recur (update seen y (fnil inc 0))
+               ys
+               (if (= (get seen y) n)
+                 (cons y result-seq)
+                 result-seq))
+        result-seq))))
 (defn main []
   (println "Введите элементы списка (через пробел), затем n:")
   (let [input (read-line)
